907cb8c9a8628479352364564fdac14064df346b
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2020-10-10 22:42:05 -0300

    fin clase nodemon y pm2

---
+++ conceptos/entorno.js
@@ -0,0 +1,5 @@
+const nombre = process.env.NOMBRE || "sin nombre";
+const web = process.env.WEB || "sin web";
+
+console.log("Hola " + nombre);
+console.log("Mi pagina: " + web);
---
+++ conceptos/monohilo.js
@@ -0,0 +1,10 @@
+console.log("Hola Mundo");
+
+let i = 0;
+
+setInterval(function () {
+  console.log(i);
+  i++;
+}, 1000);
+
+console.log("mi turno");
